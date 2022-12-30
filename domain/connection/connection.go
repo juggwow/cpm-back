9104dd902c6a7a2a9a76fd4d0da812bef891b1e2
Dirosv Eden <yodsapon.naree@icloud.com>
2022-12-30 16:32:17 +0700

    setup connection and base model

---
+++ domain/connection/connection.go
@@ -0,0 +1,19 @@
+package connection
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type DBConnection struct {
+	CPM *gorm.DB
+}
+
+type BaseModel struct {
+	ID          uint       `gorm:"column:ID"`
+	CreatedBy   string     `gorm:"column:CREATED_BY"`
+	CreatedDate *time.Time `gorm:"column:CREATED_DATE"`
+	UpdatedBy   string     `gorm:"column:UPDATED_BY"`
+	UpdatedDate *time.Time `gorm:"column:UPDATED_DATE"`
+}
