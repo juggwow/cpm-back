5437246e0efb670424efcd4231b529498a2f7499
aunzyme <aunpeac3@gmail.com>
2023-01-18 23:12:25 +0700

    Credentials and Configuration fix #11

--- domain/config/config.go
+++ domain/config/config.go
@@ -5,7 +5,6 @@ import (
 	"strings"
 
 	"github.com/spf13/viper"
-	"go.uber.org/zap"
 )
 
 var DBCpm string
@@ -15,19 +14,19 @@ func InitConfig() {
 	viper.SetDefault("db.cpm", "sqlserver://sa:yourStrongPassword@localhost:1433?database=dbname")
 
 	// set config file
-	viper.SetConfigName("config")
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath(".")
+	// viper.SetConfigName("config")
+	// viper.SetConfigType("yaml")
+	// viper.AddConfigPath(".")
 	viper.AutomaticEnv()
 	// viper.AllowEmptyEnv(true)
 	// แปลง _ underscore ใน env เป็น . dot notation ใน viper
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 	// viper.SetEnvKeyReplacer(strings.NewReplacer("_", "."))
 
-	err := viper.ReadInConfig()
-	if err != nil {
-		zap.L().Warn(fmt.Sprintf("Fatal error config file: %s \n", err))
-	}
+	// err := viper.ReadInConfig()
+	// if err != nil {
+	// 	zap.L().Warn(fmt.Sprintf("Fatal error config file: %s \n", err))
+	// }
 
 	// DBCpm = viper.GetString("db.cpm")
 	DBCpm = fmt.Sprintf("%#v", viper.AllKeys())
