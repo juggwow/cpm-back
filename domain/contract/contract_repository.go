91bc81dfed1ff6b37f95d26a858c7a4bba3dde8b
aunzyme <aunpeac3@gmail.com>
2023-01-19 22:49:27 +0700

    Fix api get work name

--- domain/contract/contract_repository.go
+++ domain/contract/contract_repository.go
@@ -7,16 +7,13 @@ import (
 
 func GetByID(db *connection.DBConnection) getByIDFunc {
 	return func(ctx context.Context, ID int) (Contract, error) {
-		result := Contract{}
-		cpm := db.CPM
+		var result Contract
+		cpm := db.CPM.Model(&result)
 		//fmt.Printf("Fatal id : %d \n", ID)
 		// cpm = cpm.Preload("EmployeeJobs.Employee")
 		// cpm = cpm.Preload("EmployeeJobs.EmployeeRole")
 		//db.Table("users").Select("users.name, emails.email").Joins("left join emails on emails.user_id = users.id").Scan(&results)
-		err := cpm.Table("[WDDEVDB\\WORKD].[CPM].[CPM].[CPM_WORK_CONTRACT] as cwc").
-			Select("cwc.ID as WORK_CONTRACT_ID , cwc.WORK_ID ,vawdd.WORK_NAME ,vawdd.WORK_TYPE_DESCRIPTION").
-			Joins("LEFT OUTER JOIN [WDDEVDB\\WORKD].[CPM].[CPM].[VIEW_ASSIGN_WORK_DESIGN_DETAIL] as vawdd ON cwc.WORK_ID = vawdd.WORK_ID").
-			Where("cwc.ID = ?", ID).
+		err := cpm.Where("WORK_CONTRACT_ID = ?", ID).
 			Scan(&result).
 			Error
 		if err != nil {
