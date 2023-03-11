61ccc1a12091644d6817265bfaac3e66c1b3bd20
aunzyme <aunpeac3@gmail.com>
2023-03-11 13:18:44 +0700

    ADD : number of item

--- domain/contract/contract_handler.go
+++ domain/contract/contract_handler.go
@@ -13,8 +13,12 @@ import (
 )
 
 type getByIDFunc func(context.Context, int) (Contract, error)
+type getNumberOfItemFunc func(context.Context, int) (NumberOfItems, error)
 
 func (fn getByIDFunc) GetByID(ctx context.Context, ID int) (Contract, error) {
+	return fn(ctx, ID)
+}
+func (fn getNumberOfItemFunc) GetNumberOfItem(ctx context.Context, ID int) (NumberOfItems, error) {
 	return fn(ctx, ID)
 }
 
@@ -35,6 +39,28 @@ func GetByIDHandler(svc getByIDFunc) echo.HandlerFunc {
 			return c.JSON(http.StatusNotFound, response.Error{Error: err.Error()})
 		}
 		data := contract.ToResponse()
+		return c.JSON(http.StatusOK, data)
+	}
+}
+
+func GetNumberOfItemHandler(svc getNumberOfItemFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		log := logger.Unwrap(c)
+
+		ID, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			log.Error(err.Error())
+			return c.JSON(http.StatusBadRequest, response.Error{Error: err.Error()})
+		}
+
+		NumberOfItems, err := svc.GetNumberOfItem(c.Request().Context(), ID)
+
+		zap.L().Sugar().Infof("work contract %d", ID)
+		if err != nil {
+			log.Error(err.Error())
+			return c.JSON(http.StatusNotFound, response.Error{Error: err.Error()})
+		}
+		data := NumberOfItems.ToResponse()
 		return c.JSON(http.StatusOK, data)
 	}
 }
