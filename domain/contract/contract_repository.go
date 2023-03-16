866adc45c4ea2558441069b9e550c5fd5b820d15
aunzyme <aunpeac3@gmail.com>
2023-03-16 12:16:15 +0700

    ADD : GET number of item from DB

--- domain/contract/contract_repository.go
+++ domain/contract/contract_repository.go
@@ -27,22 +27,22 @@ func GetByID(db *connection.DBConnection) getByIDFunc {
 func GetNumberOfItem(db *connection.DBConnection) getNumberOfItemFunc {
 	return func(ctx context.Context, ID int) (NumberOfItems, error) {
 		var result NumberOfItems
-		result = NumberOfItems{
-			All:           50,
-			AllComplete:   1,
-			AllIncomplete: 49,
-			Check:         4,
-			CheckGood:     3,
-			CheckWaste:    1,
-			Progress:      4,
-		}
-		// cpm := db.CPM.Model(&result)
-		// err := cpm.Where("WORK_CONTRACT_ID = ?", ID).
-		// 	Scan(&result).
-		// 	Error
-		// if err != nil {
-		// 	return result, err
+		// result = NumberOfItems{
+		// 	All:           50,
+		// 	AllComplete:   1,
+		// 	AllIncomplete: 49,
+		// 	Check:         4,
+		// 	CheckGood:     3,
+		// 	CheckWaste:    1,
+		// 	Progress:      4,
 		// }
+		cpm := db.CPM.Model(&result)
+		err := cpm.Where("WORK_CONTRACT_ID = ?", ID).
+			Scan(&result).
+			Error
+		if err != nil {
+			return result, err
+		}
 		return result, nil
 	}
 }
