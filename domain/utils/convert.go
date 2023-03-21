27534254bc8a6a700fe743f336f023b1a7cef820
aunzyme <aunpeac3@gmail.com>
2023-03-22 02:04:13 +0700

    Create : utils

---
+++ domain/utils/convert.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"strconv"
+	"strings"
+	"time"
+)
+
+func StringToUint(s string) uint {
+	i, _ := strconv.Atoi(s)
+	return uint(i)
+}
+
+func StringToInt(s string) int {
+	i, _ := strconv.Atoi(s)
+	return i
+}
+
+func StringToTime(s string) time.Time {
+	value := strings.Trim(s, `"`) //get rid of "
+	// if value == "" || value == "null" {
+	// 	return nil
+	// }
+
+	t, _ := time.Parse("2006-01-02", value) //parse time
+	// if err != nil {
+	// 	return err
+	// }
+	// *c = radTime(t) //set result using the pointer
+	return t
+}
+
+// func (c Time) MarshalJSON() ([]byte, error) {
+// 	return []byte(`"` + time.Time(c).Format("2006-01-02") + `"`), nil
+// 	// return []byte(`"` + time.Time(c).Format("02/01/2006") + `"`), nil
+// }
---
+++ domain/utils/response.go
@@ -0,0 +1,5 @@
+package utils
+
+type ReaponseError struct {
+	Error string `json:"error"`
+}
