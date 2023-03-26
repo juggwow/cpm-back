44ecbf3281be6ef72e0d63b18b845c3230b193cd
aunzyme <aunpeac3@gmail.com>
2023-03-27 03:18:16 +0700

    ADD : create to db

--- domain/report/create_handler.go
+++ domain/report/create_handler.go
@@ -5,25 +5,23 @@ import (
 	"cpm-rad-backend/domain/logger"
 	"cpm-rad-backend/domain/utils"
 	"errors"
+	"mime/multipart"
 	"net/http"
-	"strconv"
 	"strings"
 
 	"github.com/labstack/echo/v4"
 )
 
-type createFunc func(context.Context, Report, string) (uint, error)
+type createFunc func(context.Context, Report, File) (Report, error)
 
-func (fn createFunc) Create(ctx context.Context, r Report, createdBy string) (uint, error) {
-	return fn(ctx, r, createdBy)
+func (fn createFunc) Create(ctx context.Context, r Report, f File) (Report, error) {
+	return fn(ctx, r, f)
 }
 
 func CreateHandler(svc createFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		log := logger.Unwrap(c)
-		// var req Request
-		// log := logger.Unwrap(c)
-		// if err := c.Bind(&req); err != nil {
+
 		var r Report
 		bind(c, &r)
 
@@ -32,51 +30,23 @@ func CreateHandler(svc createFunc) echo.HandlerFunc {
 			return c.JSON(http.StatusBadRequest, utils.ReaponseError{Error: err.Error()})
 		}
 
-		docTypes := strings.Split(c.FormValue("docType"), ",")
-
 		form, _ := c.MultipartForm()
 		files := form.File["filesAttach"]
-		// filePaths := []string{}
-		var f AttachFile
-		for i, file := range files {
-			// check file type pdf and size < 50 MB
-			// src, err := file.Open()
-			// if err != nil {
-			// 	return c.JSON(http.StatusBadRequest, response.Error{Error: err.Error()})
-			// }
-			// defer src.Close()
-			// res, err := svc.FileUpload(c.Request().Context(), file, itemID)
-			// fus = append(fus, res)
-			// if err != nil {
-			// 	log.Error(err.Error())
-			// 	return c.JSON(http.StatusBadRequest, response.Error{Error: err.Error()})
-			// }
-			f.Name = file.Filename
-			f.Size = strconv.FormatInt(file.Size, 10)
-			f.Path = file.Header["Content-Type"][0]
-			f.Type = utils.StringToUint(docTypes[i])
-			r.AttachFiles = append(r.AttachFiles, f)
+
+		if err := invalidFile(files); err != nil {
+			log.Error(err.Error())
+			return c.JSON(http.StatusBadRequest, utils.ReaponseError{Error: err.Error()})
 		}
 
-		// var req Request
-		// log := logger.Unwrap(c)
-		// if err := c.Bind(&req); err != nil {
-		// 	log.Error(err.Error())
-		// 	return c.JSON(http.StatusBadRequest, response.Error{Error: err.Error()})
-		// }
+		r, err := svc.Create(c.Request().Context(), r, File{
+			Info: files,
+			Type: strings.Split(c.FormValue("docType"), ","),
+		})
 
-		// if invalidRequest(&req) {
-		// 	return c.JSON(http.StatusBadRequest, response.Error{Error: fmt.Sprint(req)})
-		// }
-
-		// // claims, _ := auth.GetAuthorizedClaims(c)
-		// // jobID, err := svc.Create(c.Request().Context(), reqJob, claims.EmployeeID)
-		// formID, err := svc.Create(c.Request().Context(), req, req.CreateBy)
-		// if err != nil {
-		// 	log.Error(err.Error())
-		// 	return c.JSON(http.StatusInternalServerError, response.Error{Error: err.Error()})
-		// }
-
+		if err != nil {
+			log.Error(err.Error())
+			return c.JSON(http.StatusInternalServerError, utils.ReaponseError{Error: err.Error()})
+		}
 		return c.JSON(http.StatusCreated, r)
 	}
 }
@@ -104,5 +74,16 @@ func invalidRequest(r *Report) error {
 		return errors.New(":Invalid Data Request")
 	}
 
+	return nil
+}
+
+func invalidFile(files []*multipart.FileHeader) error {
+	var LIMIT int64
+	LIMIT = 52428800
+	for _, file := range files {
+		if ("application/pdf" != file.Header["Content-Type"][0]) && (LIMIT < file.Size) {
+			return errors.New(":Invalid File Request")
+		}
+	}
 	return nil
 }
--- domain/report/report_repository.go
+++ domain/report/report_repository.go
@@ -3,56 +3,92 @@ package report
 import (
 	"context"
 	"cpm-rad-backend/domain/connection"
+	"cpm-rad-backend/domain/minio"
+	"cpm-rad-backend/domain/utils"
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	"github.com/inhies/go-bytesize"
+	"gorm.io/gorm"
 )
 
-func Create(db *connection.DBConnection) createFunc {
-	return func(ctx context.Context, r Report, createdBy string) (uint, error) {
-		var formID uint
+func Create(db *connection.DBConnection, m minio.Client) createFunc {
+	return func(ctx context.Context, r Report, f File) (Report, error) {
 
 		// getEmpFromDB := employee.GetByID(db)
 		// getEmpByID := func(empID string) (employee.Employee, error) {
 		// 	return getEmpFromDB(ctx, empID)
 		// }
 
-		// err := db.CPM.Transaction(func(tx *gorm.DB) error {
-		// 	radForm := r.ToModel()
-		// 	filesAttach := r.FilesAttach
+		err := db.CPM.Transaction(func(tx *gorm.DB) error {
+			report := r.ToModel()
 
-		// 	radForm.RadNo = fmt.Sprintf("rad-%d", radForm.ItemID)
-		// 	// now := time.Now()
-		// 	// radForm.CreatedDate = &now
-		// 	radForm.CreateBy = createdBy
-		// 	// radForm.UpdatedDate = &now
-		// 	// radForm.UpdatedBy = createdBy
+			report.RadNo = fmt.Sprintf("rad-%d", report.ItemID)
+			// now := time.Now()
+			// report.CreatedDate = &now
+			report.CreateBy = "createdBy"
+			// report.UpdatedDate = &now
+			// report.UpdatedBy = createdBy
 
-		// 	// if err := tx.Omit("EmployeeJobs", "JobStatus").Create(&radForm).Error; err != nil {
-		// 	// 	return err
-		// 	// }
-		// 	if err := tx.Omit("UpdateBy", "UpdateDate").Create(&radForm).Error; err != nil {
-		// 		return err
-		// 	}
-		// 	formID = radForm.ID
+			// if err := tx.Omit("EmployeeJobs", "JobStatus").Create(&report).Error; err != nil {
+			// 	return err
+			// }
 
-		// 	for _, fileAttach := range filesAttach {
-		// 		file := fileAttach.ToModel(formID, createdBy)
-		// 		if err := tx.Create(&file).Error; err != nil {
-		// 			return err
-		// 		}
-		// 	}
+			if err := tx.Omit("UpdateBy", "UpdateDate").Create(&report).Error; err != nil {
+				return err
+			}
 
-		// 	// if err := updateJobEmployees(tx, formID, &req, createdBy, getEmpByID); err != nil {
-		// 	// 	return err
-		// 	// }
+			for i, file := range f.Info {
+				src, err := file.Open()
+				if err != nil {
+					return err
+				}
+				defer src.Close()
 
-		// 	// deptChangeCode := ""
-		// 	// if err := tx.Model(&employee_job.EmployeeJob{}).Joins("Employee").Joins("EmployeeRole").Where(
-		// 	// 	"CMDC_JOB_ID = ? AND EmployeeRole.ROLE_NAME_ENG = ?", formID, employee_role.SUPERVISOR,
-		// 	// ).Pluck("Employee.DEPT_CHANGE_CODE", &deptChangeCode).Error; err != nil {
-		// 	// 	return err
-		// 	// }
-		// 	return nil
-		// })
+				info, objectName, err := m.Upload(ctx, file, fmt.Sprintf("%d/%d", report.ItemID, report.ID))
+				if err != nil {
+					return err
+				}
 
-		return formID, nil
+				b := bytesize.New(float64(info.Size))
+				displaySize := b.Format("%.2f ", "", false)
+				words := strings.Fields(displaySize)
+
+				f := AttachFile{
+					ID:          0,
+					Name:        file.Filename,
+					ObjectName:  objectName,
+					DisplaySize: displaySize,
+					Size:        words[0],
+					Unit:        words[1],
+					Path:        info.Key,
+					FileType:    strings.Replace(filepath.Ext(info.Key), ".", "", 1),
+					DocType:     utils.StringToUint(f.Type[i]),
+				}
+
+				r.AttachFiles = append(r.AttachFiles, f)
+				file := f.ToModel(report)
+				if err := tx.Create(&file).Error; err != nil {
+					//remove file minio
+					return err
+				}
+			}
+
+			// if err := updateJobEmployees(tx, formID, &req, createdBy, getEmpByID); err != nil {
+			// 	return err
+			// }
+
+			// deptChangeCode := ""
+			// if err := tx.Model(&employee_job.EmployeeJob{}).Joins("Employee").Joins("EmployeeRole").Where(
+			// 	"CMDC_JOB_ID = ? AND EmployeeRole.ROLE_NAME_ENG = ?", formID, employee_role.SUPERVISOR,
+			// ).Pluck("Employee.DEPT_CHANGE_CODE", &deptChangeCode).Error; err != nil {
+			// 	return err
+			// }
+
+			return nil
+		})
+
+		return r, err
 	}
 }
