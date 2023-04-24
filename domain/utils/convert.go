c07aded033cdd63041d2ece5c3a55470a43f11e7
aunzyme <aunpeac3@gmail.com>
2023-04-24 14:24:25 +0700

    add: ower time type

--- domain/utils/convert.go
+++ domain/utils/convert.go
@@ -6,6 +6,8 @@ import (
 	"time"
 )
 
+type Time time.Time
+
 func StringToUint(s string) uint {
 	i, _ := strconv.Atoi(s)
 	return uint(i)
@@ -30,7 +32,21 @@ func StringToTime(s string) time.Time {
 	return t
 }
 
-// func (c Time) MarshalJSON() ([]byte, error) {
-// 	return []byte(`"` + time.Time(c).Format("2006-01-02") + `"`), nil
-// 	// return []byte(`"` + time.Time(c).Format("02/01/2006") + `"`), nil
-// }
+func (c *Time) UnmarshalJSON(b []byte) error {
+	value := strings.Trim(string(b), `"`) //get rid of "
+	if value == "" || value == "null" {
+		return nil
+	}
+
+	t, err := time.Parse("2006-01-02", value) //parse time
+	if err != nil {
+		return err
+	}
+	*c = Time(t) //set result using the pointer
+	return nil
+}
+
+func (c Time) MarshalJSON() ([]byte, error) {
+	return []byte(`"` + time.Time(c).Format("2006-01-02") + `"`), nil
+	// return []byte(`"` + time.Time(c).Format("02/01/2006") + `"`), nil
+}
