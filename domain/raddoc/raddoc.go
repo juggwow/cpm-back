ee008f67c5a868f77c1888e62f97fbafcc1ed0e5
aunzyme <aunpeac3@gmail.com>
2023-08-08 18:59:52 +0700

    EDIT : type RadDoc

--- domain/raddoc/raddoc.go
+++ domain/raddoc/raddoc.go
@@ -28,9 +28,9 @@ type RadDoc struct {
 	Qty        string   `json:"qty" gorm:"column:DELIVERED_QTY"`
 	Arrival    rad.Time `json:"arrival" gorm:"column:ARRIVAL_DATE_AT_SITE"`
 	Inspection rad.Time `json:"inspection" gorm:"column:INSPECTION_DATE"`
-	CreateBy   string   `json:"CreateBy" gorm:"column:CREATED_BY"`
-	StateID    uint     `json:"StateID" gorm:"column:STATE_ID"`
-	StateName  string   `json:"StateName" gorm:"column:STATE_NAME"`
+	CreateBy   string   `json:"createBy" gorm:"column:CREATED_BY"`
+	StateID    uint     `json:"stateID" gorm:"column:STATE_ID"`
+	StateName  string   `json:"stateName" gorm:"column:STATE_NAME"`
 }
 type listOfDoc []RadDoc
 
